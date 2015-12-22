40fffd7ae5b933609a2834fadcca1eceead0b1d3
sahratilki <sahratilki17@gmail.com>
2015-12-22 13:24:56 +0200

    note.m düzenlendi

--- note.m
+++ note.m
@@ -1,7 +1,9 @@
-function [xx , tt] = note ( f,dur )
-
-ff=2*pi*f
-tt=0:1/(ff*1000):dur;
-xx = sin (2*pi*f*t);
-
+function [xx,tt]=note(frekans,vurus) 
+Fs=8192;
+tt=0:1/Fs:((0.25-(1/Fs))/2); 
+zz=0;
+   xx=sin(2*pi*frekans*tt); 
+if length(xx)==length(zz)    
+       zz=[linspace(0,1.5,Fs*vurus*(2/8)),linspace(1.5,1,Fs*vurus*(1/8)),linspace(1,1,Fs*vurus*(4/8)),linspace(1,0,Fs*vurus*(1/8))];
+end
 end
